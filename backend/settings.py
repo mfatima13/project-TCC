949d6d9806902937c1790d81b7c742ebff91ddb0
mariadefatima <mariadefatimababosa@outlook.com>
2020-12-07 09:41:28 -0300

    modifying debug on .env

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['ksproject-api.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['ksproject-api.herokuapp.com/']
 
 # Application definition
 
